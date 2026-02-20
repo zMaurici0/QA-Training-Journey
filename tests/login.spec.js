0991b01c2140fb4bd4c1d40909e7b3517827bc2a
zMaurici0 <zmauriciomota@gmail.com>
2026-02-20 13:17:40 -0400

    Removendo page.pause(), page.close(), test.only, adicionando test.describe e fazendo testes de erro

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,35 +2,63 @@ import {test, expect} from '@playwright/test'
 
 // Testando Login em diferentes sites
 
-test.only('Login test 1', async({page}) => {
+test('Login válido site 1', async({page}) => {
     await page.goto('https://demo.applitools.com/');
-    await page.pause()
-    await page.getByPlaceholder('Enter your username').fill('Mauricio')
-    await page.getByPlaceholder('Enter your password').fill('123321')
-
-    await page.waitForSelector('text=Sign in', {timeout: 5000});
+    await page.getByPlaceholder('Enter your username').fill('Mauricio');
+    await page.getByPlaceholder('Enter your password').fill('123321');
     await page.getByText('Sign in').click();
-    await page.close();
+
+    await expect(page).toHaveURL('https://demo.applitools.com/app.html'); //verificando se o login deu certo 
 })
 
-test('Login test 2', async({page}) => {
-    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
-    await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
-    await page.getByRole('button', { name: 'Login' }).click();
-    await page.getByRole('listitem').filter({ hasText: 'aliaa astal' }).locator('i').click();
-    await page.getByRole('menuitem', { name: 'Logout' }).click();
-    await page.close();
+
+test.describe('Testes login - OrangeHTM', () => {
+
+    test('Login válido', async({page}) => {
+        await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+        await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+        await page.getByRole('button', { name: 'Login' }).click();
+
+        await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index'); //verificando se o login deu certo 
+    })
+
+    test('Login inválido', async ({page}) => {
+        await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        await page.getByPlaceholder('Username').fill('errado');
+        await page.getByPlaceholder('Password').fill('errado');
+        await page.getByRole('button', { name: 'Login' }).click();
+
+        await expect(page.getByText('Invalid credentials')).toBeVisible(); //verificando se o login deu errado
+        await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'); //garante que não redirecionou
+    });
+
 })
 
-test('Login test 3', async({page}) => {
-    await page.pause();
-    await page.goto('https://admin-demo.nopcommerce.com/login?returnUrl=%2Fadmin%2F');
-    await page.getByRole('textbox', { name: 'Email:' }).press('Control+a');
-    await page.getByRole('textbox', { name: 'Email:' }).fill('admin@yourstore.com');
-    await page.getByRole('textbox', { name: 'Password:' }).press('Control+a');
-    await page.getByRole('textbox', { name: 'Password:' }).fill('admin');
-    await page.getByRole('button', { name: 'Log in' }).click();
-    await page.getByRole('link', { name: 'Logout' }).click();
-    await page.close();
+test.describe('Testes login - nopCommerce', () => {
+
+    test('Login válido', async({page}) => {
+        await page.goto('https://admin-demo.nopcommerce.com/login?returnUrl=%2Fadmin%2F');
+        await page.getByRole('textbox', { name: 'Email:' }).press('Control+a');
+        await page.getByRole('textbox', { name: 'Email:' }).fill('admin@yourstore.com');
+        await page.getByRole('textbox', { name: 'Password:' }).press('Control+a');
+        await page.getByRole('textbox', { name: 'Password:' }).fill('admin');
+        await page.getByRole('button', { name: 'Log in' }).click();
+
+        await expect(page).toHaveURL('https://admin-demo.nopcommerce.com/admin/'); //verificando se o login deu certo 
+    })
+
+
+    test('Login inválido', async({page}) => {
+        await page.goto('https://admin-demo.nopcommerce.com/login?returnUrl=%2Fadmin%2F');
+        await page.getByRole('textbox', { name: 'Email:' }).press('Control+a');
+        await page.getByRole('textbox', { name: 'Email:' }).fill('erro@gmail.com')
+        await page.getByRole('textbox', { name: 'Password:' }).press('Control+a');
+        await page.getByRole('textbox', { name: 'Password:' }).fill('erro');
+        await page.getByRole('button', { name: 'Log in' }).click();
+
+        await expect(page.getByText('Login was unsuccessful. Please correct the errors and try again.No customer account found')).toBeVisible();
+        await expect(page).toHaveURL('https://admin-demo.nopcommerce.com/login?returnurl=%2Fadmin%2F'); //garante que não redirecionou
+    })
+
 })
